16352367d7980dae5f8548dab951163a69214f22
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-24 17:06:39 -0400

    Create Rover class

---
+++ Rover.rb
@@ -0,0 +1,21 @@
+class Rover
+
+  def initialize
+    @x_coordinate
+    @y_coordinate
+    @direction
+  end
+
+  def read_instruction
+
+  end
+
+  def move
+
+  end
+
+  def turn
+    
+  end
+
+end
