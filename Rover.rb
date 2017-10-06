8a472b3cbef763351b47bcebe861f0660364357e
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-06 18:57:26 -0400

    ajust variables of turn method, remaned move method and edit read_instruction method

--- Rover.rb
+++ Rover.rb
@@ -4,50 +4,65 @@ class Rover
     @x_coordinate = x_coordinate
     @y_coordinate = y_coordinate
     @direction = direction
+
+    @counter = 0
   end
 
-  def read_instruction(x, y, direction)
-    @x_coordinate = x
-    @y_coordinate = y
-    @direction = direction
+  def input(x_coordinate, y_coordinate, start_direction)
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+    @direction = start_direction
   end
 
-  def move
-    if direction == "N"
+  def read_instruction(instructions)
+
+    orders = instructions.chars
+
+    orders.each do |order|
+      if order == "M"
+        move?
+      else
+        turn(order)
+      end
+    end
+  end
+
+  def move?
+    if @direction == "N"
       @y_coordinate += 1
-    elsif direction == "S"
+    elsif @direction == "S"
       @y_coordinate -= 1
-    elsif direction == "E"
+    elsif @direction == "E"
       @x_coordinate += 1
-    elsif direction == "W"
+    elsif @direction == "W"
       @x_coordinate -= 1
     end
   end
 
   def turn(direction)
-    counter = 0
-
-    if counter > 4
-      counter = 0
-    elsif counter < 0
-      counter = 4
+    if @counter > 4
+      @counter = 0
+    elsif @counter < 0
+      @counter = 4
     end
 
     if direction == "L"
-      counter -= 1
+      @counter -= 1
     elsif direction == "R"
-      coutner += 1
+      @coutner += 1
     end
 
-    if counter == 0
+    if @counter == 0
       @direction = "N"
-    elsif counter == 1
+    elsif @counter == 1
       @direction = "E"
-    elsif counter == 2
+    elsif @counter == 2
       @direction = "S"
-    elsif counter == 3
+    elsif @counter == 3
       @direction = "W"
     end
+
+    @counter
   end
 
   def output
@@ -56,4 +71,6 @@ class Rover
 end
 
 rover = Rover.new(1, 2, "N")
+puts rover.output
+rover.read_instruction("LMLMLMLMM")
 puts rover.output
